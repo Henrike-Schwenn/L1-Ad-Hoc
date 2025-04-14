54bf0fc79b6163dbca1a374ab8c72b68a3445c47
Henrike Schwenn <henrike.schwenn@gmail.com>
2025-04-14 16:56:17 +0200

    Aufgabe 5 funktioniert

--- ArrayAddressBook.java
+++ ArrayAddressBook.java
@@ -1,8 +1,9 @@
 
 public class ArrayAddressBook {
-    Person [] persons = new Person[1]; //Default-Größe 1 wird verwendet, wenn im Konstruktor eine Größe kleiner 1 angegeben wird
+    Person [] persons = new Person[]; //Default-Größe 1 wird verwendet, wenn im Konstruktor eine Größe kleiner 1 angegeben wird
 
     public ArrayAddressBook(Person [] persons) {
+        this.persons.length = persons.length;
     }
 
     
--- Aufgabe5.java
+++ Aufgabe5.java
@@ -1,10 +1,17 @@
+
+import java.util.Arrays;
+
 public class Aufgabe5 {
+
     public static void main (String [] args) {
         int [][] arr1D = new int [10][];
         for (int i = 0; i < arr1D.length; i = i + 1){
+            int [] arr2D = new int [i+1];
             for (int j = 0; j <= i; j = j + 1){
-                arr1D [i] [j] = j;
+                arr2D [j] = j;
             }
+        arr1D[i] = arr2D;
+        System.out.println(Arrays.toString(arr1D[i]));
         }
 
     }
